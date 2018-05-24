c83503deff472d8f92d6a547c02a4f80f29a5634
Abelatox <abel54225@gmail.com>
2018-05-24 16:26:41 +0200

    Login mig funciona

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,15 +1,17 @@
 package application;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -17,23 +19,48 @@ public class LoginController {
 
 	@FXML
 	private Button btnAccedir;
+	
+	@FXML
+	private TextField tfUsuari, tfContrassenya;
 
 	@FXML
 	void btnAccedir(ActionEvent event) throws Exception {
-		Pane root = FXMLLoader.load(getClass().getResource("/application/MainController.fxml"));
-		Scene scene = new Scene(root);
-		Stage stage = (Stage) btnAccedir.getScene().getWindow();
-		Util.openGUI(scene, stage, Strings.TITLE_MAIN);
+		if (validarLogin(tfUsuari.getText(), tfContrassenya.getText())) {
+			Pane root = FXMLLoader.load(getClass().getResource("/application/MainController.fxml"));
+			Scene scene = new Scene(root);
+			Stage stage = (Stage) btnAccedir.getScene().getWindow();
+			Util.openGUI(scene, stage, Strings.TITLE_MAIN);
+		}
 	}
-/*
-	@FXML
-	void cmdAdmin(ActionEvent event) throws Exception {
-		Pane root = FXMLLoader.load(getClass().getResource("/application/admin/MainAdmin.fxml"));
-		Scene scene = new Scene(root);
-		Stage stage = (Stage) btnAccedir.getScene().getWindow();
-		Util.openGUI(scene, stage, Strings.TITLE_MAIN);
+
+	private boolean validarLogin(String nick, String pass) {
+		try {
+			Statement st = Main.getConnection().createStatement();
+			ResultSet rs = st.executeQuery(" select password from treballador where nick = '"+nick+"' ");
+			if(rs.next()) {
+				System.out.println("usuari trobat, comprovant password");
+				if(pass.equals(rs.getString("password"))){
+					System.out.println("Password correcta, benvingut");
+				} else {
+					
+				}
+
+			} else {
+				System.out.println("Usuari no existeix");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
-*/
+
+	/*
+	 * @FXML void cmdAdmin(ActionEvent event) throws Exception { Pane root =
+	 * FXMLLoader.load(getClass().getResource("/application/admin/MainAdmin.fxml"));
+	 * Scene scene = new Scene(root); Stage stage = (Stage)
+	 * btnAccedir.getScene().getWindow(); Util.openGUI(scene, stage,
+	 * Strings.TITLE_MAIN); }
+	 */
 	@FXML
 	void cmdAbout(ActionEvent event) throws Exception {
 		System.out.println("Showing About");
@@ -50,9 +77,9 @@ public class LoginController {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void initialize() {
-		
+
 	}
 
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,31 +1,51 @@
 package application;
-	
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
 	static Scene scene;
+	static Connection con;
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Class.forName("org.postgresql.Driver");
+			//con = DriverManager.getConnection("jdbc:postgresql://192.168.1.93/perruqueria", "postgres", "dam");
+			con = DriverManager.getConnection("jdbc:postgresql://localhost/perruqueria", "postgres", "smx");
+			
+			// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			Pane root = FXMLLoader.load(getClass().getResource("/application/LoginController.fxml"));
 			Scene scene = new Scene(root);
 			Util.openGUI(scene, primaryStage, Strings.TITLE_MAIN);
-		} catch(Exception e) {
+		}catch(IOException e){
+			System.out.println("Problema accedint al FXML");
+			e.printStackTrace();
+		} catch (SQLException e) {
+			System.out.println("Problema amb SQL");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			System.out.println("Classe no trobada");
 			e.printStackTrace();
 		}
 	}
 	
+	public static Connection getConnection() {
+		return con;
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	public static Scene getMainScene() {
 		return scene;
 	}
